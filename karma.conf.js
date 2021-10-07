396761abc1c50e4d97b6a6b2e917e32242beefed
Ashley Bloxom <abloxom@chathamfinancial.com>
2021-10-07 14:06:54 -0400

    refactor: change project name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cypress-example-app'),
+      dir: require('path').join(__dirname, './coverage/shadow-dom-problem'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe("AppComponent", () => {
     expect(app).toBeTruthy();
   });
 
-  it("should have as title 'cypress-example-app'", () => {
+  it("should have as title 'shadow-dom-problem'", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("cypress-example-app");
+    expect(app.title).toEqual("shadow-dom-problem");
   });
 
   it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector(".content span").textContent).toContain("cypress-example-app app is running!");
+    expect(compiled.querySelector(".content span").textContent).toContain("shadow-dom-problem app is running!");
   });
 });
